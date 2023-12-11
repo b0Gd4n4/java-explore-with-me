adb3308a84719c1004b627800a2d0b54a516b64c
Bogdan <ggbogdanov-03@mail.ru>
2023-12-11 15:26:38 +1000

    main_svc #1.9

--- ewm-service/src/main/java/ru/practicum/request/dto/RequestDto.java
+++ ewm-service/src/main/java/ru/practicum/request/dto/RequestDto.java
@@ -3,10 +3,8 @@ package ru.practicum.request.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-import ru.practicum.dto.marker.Marker;
 import ru.practicum.enums.Status;
 
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 
@@ -16,7 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class RequestDto {
-    @NotNull(groups = Marker.OnUpdate.class)
+
     Long id;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
